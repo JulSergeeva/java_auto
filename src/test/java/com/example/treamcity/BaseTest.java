7eff888cc369e37a66349b6028e6449926bb46fc
JulSergeeva <sergeevajulija@bk.ru>
2025-03-01 21:48:21 +0400

    Add TestData class/ TestDataGenerator

--- src/test/java/com/example/treamcity/BaseTest.java
+++ src/test/java/com/example/treamcity/BaseTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 
+import static com.example.teamcity.generators.TestDataGenerator.generate;
+
 public class BaseTest {
     protected SoftAssert softy;
     protected CheckedRequest superUserCheckRequests = new CheckedRequest(Specifications.superUserSpec());
@@ -18,5 +20,11 @@ public class BaseTest {
     @AfterMethod(alwaysRun = true)
     public void afterTest() {
         softy.assertAll();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void beforeTest() {
+        softy = new SoftAssert();
+        testData = generate();
     }
 }
