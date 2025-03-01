d3f1ac9cc975cce98c31b40133a8a0b1ad407b39
JulSergeeva <sergeevajulija@bk.ru>
2025-03-01 23:38:47 +0400

    update test on Project

--- src/test/java/com/example/treamcity/api/ProjectTest.java
+++ src/test/java/com/example/treamcity/api/ProjectTest.java
@@ -1,12 +1,11 @@
-package com.example.teamcity.api;
+package com.example.treamcity.api;
 
 import com.example.teamcity.enums.Endpoint;
 import com.example.teamcity.models.Project;
 import com.example.teamcity.models.User;
-import com.example.teamcity.requests.checked.CheckedBase;
+import com.example.teamcity.requests.CheckedRequest;
 import com.example.teamcity.requests.unchecked.UncheckedBase;
 import com.example.teamcity.spec.Specifications;
-import com.example.treamcity.api.BaseApiTest;
 import org.apache.hc.core5.http.HttpStatus;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
@@ -14,8 +13,6 @@ import org.testng.asserts.SoftAssert;
 
 import static com.example.teamcity.enums.Endpoint.*;
 import static com.example.teamcity.generators.TestDataGenerator.generate;
-import static io.qameta.allure.Allure.step;
-import com.example.teamcity.requests.CheckedRequest;
 
 @Test(groups = {"Regression"})
 public class ProjectTest extends BaseApiTest {
@@ -23,12 +20,10 @@ public class ProjectTest extends BaseApiTest {
     @Test(description = "User should be able to create a project", groups = {"Positive", "CRUD"})
     public void userCreatesProjectTest() {
         var user = generate(User.class);
-
         var superUserRequests = new CheckedRequest(Specifications.superUserSpec());
         superUserRequests.getRequest(USERS).create(user);
 
         var userRequests = new CheckedRequest(Specifications.authSpec(user));
-
         var project = generate(Project.class);
 
         superUserRequests.getRequest(ROLES).assign(user.getUsername(), "PROJECT_ADMIN", "_Root");
@@ -36,14 +31,12 @@ public class ProjectTest extends BaseApiTest {
         project = userRequests.<Project>getRequest(PROJECTS).create(project);
 
         Project createdProject = (Project) userRequests.getRequest(PROJECTS).read(project.getId());
-
         softy.assertEquals(project.getName(), createdProject.getName(), "Project name is not correct");
     }
 
     @Test(description = "User should not be able to create two projects with the same id", groups = {"Negative", "CRUD"})
     public void userCreatesTwoProjectsWithTheSameIdTest() {
         var user = generate(User.class);
-
         var superUserRequests = new CheckedRequest(Specifications.superUserSpec());
         superUserRequests.getRequest(USERS).create(user);
 
@@ -69,7 +62,6 @@ public class ProjectTest extends BaseApiTest {
     @Test(description = "User should not be able to create a project without a name", groups = {"Negative"})
     public void createProjectWithoutNameTest() {
         var user = generate(User.class);
-
         var superUserRequests = new CheckedRequest(Specifications.superUserSpec());
         superUserRequests.getRequest(USERS).create(user);
 
@@ -79,7 +71,7 @@ public class ProjectTest extends BaseApiTest {
         project.setName(null);
 
         userRequests.getRequest(PROJECTS).createWithError(project, HttpStatus.SC_BAD_REQUEST);
-
+        
         new UncheckedBase(Specifications.authSpec(user), PROJECTS)
                 .create(project)
                 .then()
